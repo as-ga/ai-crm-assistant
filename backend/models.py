4c119158763c620920e656aadf29012da8b3594e
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2026-06-27 14:42:09 +0530

    remove uniqueness constraint from user name field in the User model

--- backend/models.py
+++ backend/models.py
@@ -16,7 +16,7 @@ class User(Base, TimestampMixin):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    name: Mapped[str] = mapped_column(String, unique=True, index=True)
+    name: Mapped[str] = mapped_column(String, index=True)
     email: Mapped[str] = mapped_column(String, unique=True, index=True)
     password: Mapped[str] = mapped_column(String)
 
